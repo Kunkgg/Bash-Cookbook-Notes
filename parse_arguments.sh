be3d226509f8fa9d231804b5e59d56787321da15
gk07 <gk_520@hotmail.com>
2021-11-11 06:35:30 +0800

    Parse arguments

---
+++ parse_arguments.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Usage of getopts
+
+aflag=
+bflag=
+while getopts 'ab:' OPTION
+do
+    case $OPTION in
+        a) aflag=1
+            ;;
+        b) bflag=1
+            bval="$OPTARG"
+            ;;
+        ?) printf "Usage: %s: [-a] [-b value] args\n" ${0##*/} >&2
+            exit 2
+            ;;
+    esac
+done
+
+shift $(($OPTIND - 1))
+
+if [ "$aflag" ]; then
+    printf "Option -a specified\n"
+fi
+if [ "$bflag" ]; then
+    printf 'Option -b "%s" specified\n' "$bval"
+fi
+printf 'Remaining arguments are: "%s"\n' "$*"
